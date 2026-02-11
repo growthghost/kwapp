4ec4aed0152d791a58d1108cda70e27a6979d2fb
growthghost <rknipe77@gmail.com>
2026-02-10 20:51:59 -0600

    Layout.js_Fix header bar and nav

--- web/src/app/layout.js
+++ web/src/app/layout.js
@@ -1,3 +1,4 @@
+// web/src/app/layout.js
 import "./globals.css";
 
 export const metadata = {
@@ -9,17 +10,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header className="rbTopbar">
-          <div className="rbBrand">
-            <img
-              className="rbLogo"
+        <header className="rb-header">
+          <div className="rb-header-inner">
+            <div className="rb-brand">
+              <img
+                className="rb-logo"
                 src="/RankedBoxLogoIcon.png"
-                  alt="RankedBox"
-                  width={34}
-                  height={34}
-              style={{ width: 34, height: 34, objectFit: "contain", display: "block" }}
-            />
-            <span className="rbBrandText">RankedBox</span>
+                alt="RankedBox"
+                width={34}
+                height={34}
+                style={{ width: 34, height: 34, objectFit: "contain", display: "block" }}
+              />
+              <span className="rb-brand-name">RankedBox</span>
+            </div>
+
+            <nav className="rb-nav">
+              <a href="/try">Try</a>
+              <a href="/app">App</a>
+              <a href="/pricing">Pricing</a>
+              <a href="/login">Login</a>
+            </nav>
           </div>
         </header>
 
